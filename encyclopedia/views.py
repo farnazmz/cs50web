022f98193707c7245213878404826b0b1f50ef8a
farna <farna@DESKTOP-MBKCKEA>
2022-03-10 14:10:01 -0800

    2

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
 from . import util
+import markdown2
+from markdown2 import Markdown
+
 
 
 def index(request):
@@ -8,3 +10,18 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    get_title_md = util.get_entry(title)
+
+    if get_title_md == None:
+        return render(request, "encyclopedia/error.html"), {
+            "title": get_title_md
+        }
+
+    else:
+        markdowner = Markdown()
+        get_title_html = markdowner.convert(get_title_md)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": get_title_html
+        })
